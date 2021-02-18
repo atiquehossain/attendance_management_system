01e678064dcac122ce399322317bbcae3e140195
atiquehossain <atique.hossain@northsouth.edu>
2021-02-17 21:12:25 -0800

    Login and edit ui(Admin)

---
+++ app/src/main/java/com/example/ams/addAttendance.java
@@ -0,0 +1,14 @@
+package com.example.ams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addAttendance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_attendance);
+    }
+}
