bf63ad99debac22275beff3106b98c108a277dcb
atiquehossain <atique.hossain@northsouth.edu>
2021-03-29 05:03:35 -0700

    onBindViewHolder

--- app/src/main/java/com/example/ams/AdapterForEmployeeHome.java
+++ app/src/main/java/com/example/ams/AdapterForEmployeeHome.java
@@ -3,6 +3,7 @@ package com.example.ams;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -33,18 +34,47 @@ public class AdapterForEmployeeHome extends RecyclerView.Adapter<AdapterForEmplo
         Date newcheckout= userList.get(position).getE_m_l_chekout();
         Date newLate=userList.get(position).getE_m_l_late();
         String newStatus=userList.get(position).getE_m_l_status();
+        
+        
+        holder.setData(newDate,newcheckin,newcheckout,newLate,newStatus);
 
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return userList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
+
+        private TextView textviewone;
+        private TextView textviewtwo;
+        private TextView textviewthree;
+        private TextView textviewfour;
+        private TextView textviewfive;
+
+
+
+
+        public ViewHolder(@NonNull View itemView)
+        {
             super(itemView);
+        textviewone=itemView.findViewById(R.id.e_m_l_date);
+        textviewtwo=itemView.findViewById(R.id.e_m_l_chekin);
+        textviewthree=itemView.findViewById(R.id.e_m_l_chekout);
+        textviewfour=itemView.findViewById(R.id.e_m_l_late);
+        textviewfive=itemView.findViewById(R.id.e_m_l_status);
+        }
+
+
+
+        public void setData(Date newDate, Date newcheckin, Date newcheckout, Date newLate, String newStatus) {
+            textviewone.setText(newDate.toString());
+            textviewtwo.setText(newcheckin.toString());
+            textviewthree.setText(newcheckout.toString());
+            textviewfour.setText(newLate.toString());
+            textviewfive.setText(newStatus);
         }
     }
 }
--- app/src/main/java/com/example/ams/Employee_home.java
+++ app/src/main/java/com/example/ams/Employee_home.java
@@ -1,14 +1,38 @@
 package com.example.ams;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
 
 public class Employee_home extends AppCompatActivity {
+    RecyclerView recyclerView;
+    LinearLayoutManager layoutManager;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_home);
+
+
+        initData();
+        initRecylerView();
     }
+
+    private void initRecylerView() {
+
+         recyclerView = findViewById(R.id.employee_home_recycleview);
+         layoutManager=new LinearLayoutManager(this);
+         layoutManager.setOrientation(RecyclerView.VERTICAL);
+         recyclerView.setLayoutManager(layoutManager);
+         adapter=new AdapterForEmployeeHome(userList);
+
+
+    }
+
+
 }
