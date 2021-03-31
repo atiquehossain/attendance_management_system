f74c2127c353d123d9c21a74432201e8b84be3ae
Md. Atique Hossain <atique.hossain@northsouth.edu>
2021-03-31 01:22:33 -0700

    adapter

--- app/src/main/java/com/example/ams/Employee_home.java
+++ app/src/main/java/com/example/ams/Employee_home.java
@@ -1,6 +1,7 @@
 package com.example.ams;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,34 +15,50 @@ import java.util.List;
 public class Employee_home extends AppCompatActivity {
     RecyclerView recyclerView;
     LinearLayoutManager layoutManager;
-    Adapter adapter;
     List<ModelClassForEmployeeHome> userList;
+    private AdapterForEmployeeHome adapterForEmployeeHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_home);
 
-
         initData();
+        adapterForEmployeeHome = new AdapterForEmployeeHome(userList);
+
         initRecylerView();
     }
 
     private void initData() {
 
-        userList=new ArrayList<>();
-        userList.add(new ModelClassForEmployeeHome("10-10-20","10.24","11.00","10.24","present"));
+        userList = new ArrayList<>();
+        userList.add(new ModelClassForEmployeeHome("10-11-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-12-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-13-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-11-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-12-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-13-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-11-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-12-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-13-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-11-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-12-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-13-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-11-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-12-20", "10.24", "11.00", "10.24", "present"));
+        userList.add(new ModelClassForEmployeeHome("10-13-20", "10.24", "11.00", "10.24", "present"));
     }
 
     private void initRecylerView() {
 
-         recyclerView = findViewById(R.id.employee_home_recycleview);
-         layoutManager=new LinearLayoutManager(this);
-         layoutManager.setOrientation(RecyclerView.VERTICAL);
-         recyclerView.setLayoutManager(layoutManager);
-         adapter= (Adapter) new AdapterForEmployeeHome(userList);
-         recyclerView.setAdapter((RecyclerView.Adapter) adapter);
-         ((RecyclerView.Adapter) adapter).notifyDataSetChanged();
+
+        recyclerView = findViewById(R.id.employee_home_recycleview);
+        layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapterForEmployeeHome);
 
 
     }
