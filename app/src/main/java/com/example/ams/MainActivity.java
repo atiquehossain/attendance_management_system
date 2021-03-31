0134e108fea5169c3946e0aba920b4e13444382c
Md. Atique Hossain <atique.hossain@northsouth.edu>
2021-03-31 03:33:02 -0700

    validation

--- app/src/main/java/com/example/ams/MainActivity.java
+++ app/src/main/java/com/example/ams/MainActivity.java
@@ -21,37 +21,38 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
 
         email = findViewById(R.id.emailname);
-        password=findViewById(R.id.password);
-
-
+        password = findViewById(R.id.password);
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                
-                if(validation()==true){
+                if (validation()) {
                     openNewActivity();
                 }
             }
-
         });
 
-        
+
     }
-    public void openNewActivity(){
+
+    public void openNewActivity() {
         Intent intent = new Intent(this, Employee_home.class);
         startActivity(intent);
     }
-    private boolean validation(){
+
+    private boolean validation() {
         boolean flag = true;
-        if(email.toString().isEmpty()){
+        if (email.getText().toString().isEmpty()) {
             Toast.makeText(MainActivity.this, "You must enter email  to login", Toast.LENGTH_SHORT).show();
-            flag =false;
-        } else  if( password.toString().isEmpty()){
+            flag = false;
+        } else if (email.getText().toString().length() < 4) {
+            Toast.makeText(MainActivity.this, "user name must be more then 4 char", Toast.LENGTH_SHORT).show();
+            flag = false;
+        } else if (password.getText().toString().isEmpty()) {
             Toast.makeText(MainActivity.this, "You must enter password  to login!", Toast.LENGTH_SHORT).show();
-            flag =false;
+            flag = false;
         }
 
         return flag;
